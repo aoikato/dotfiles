61a30ff7fbffa737c63bce91012d13a09fb7b5e7
aoikato <cs16018@s.inf.shizuoka.ac.jp>
2017-07-10 22:07:46 +0900

    Add .inputrc, modify script files

--- makeLinks.bat
+++ makeLinks.bat
@@ -1,3 +1,4 @@
 mklink $HOME$/.vimrc $HOME$/dotfils/.vimrc
 mklink $HOME$/.vimperatorrc $HOME$/dotfils/.vimperatorrc
 mklink $HOME$/.bashrc $HOME$/dotfils/.bashrc
+mklink $HOME$/.inputrc $HOME$/dotfils/.inputrc
--- makeLinks.sh
+++ makeLinks.sh
@@ -2,3 +2,4 @@
 ln -s ~/git/dotfiles/.vimrc ~/.vimrc
 ln -s ~/git/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
+ln -s ~/git/dotfiles/.inputrc ~/.inputrc
