47d369c9f0b562e384fce833694c9e6f10742a4b
aoikato <cs16018@s.inf.shizuoka.ac.jp>
2017-08-03 16:39:54 +0900

    # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Your branch is up-to-date with 'origin/master'.## Changes to be committed:#	new file:   .latexmkrc#	new file:   .latexmkrc.swp#	modified:   makeLinks.bat#	modified:   makeLinks.sh#Add .latexmkrc, and modify script files

--- makeLinks.bat
+++ makeLinks.bat
@@ -2,4 +2,5 @@ mklink $HOME$/.vimrc $HOME$/dotfils/.vimrc
 mklink $HOME$/.vimperatorrc $HOME$/dotfils/.vimperatorrc
 mklink $HOME$/.bashrc $HOME$/dotfils/.bashrc
 mklink $HOME$/.inputrc $HOME$/dotfils/.inputrc
+mklink $HOME$/.latexmkrc $HOME$/dotfils/.latexmkrc
 exit 0
--- makeLinks.sh
+++ makeLinks.sh
@@ -3,3 +3,4 @@ ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/git/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/git/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/git/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/git/dotfiles/.latexmkrc ~/.latexmkrc
