13297db0fe428a315cbd27c5fb5e421e397962aa
aoikato <cemsy5cmu34bsmhgpisx@docomo.ne.jp>
2017-09-17 08:16:21 +0900

    Fix several problems in batch file

--- makeLinks.bat
+++ makeLinks.bat
@@ -1,6 +1,3 @@
-mklink $HOME$/_vimrc $HOME$/dotfils/_vimrc
-mklink $HOME$/_vimperatorrc $HOME$/dotfils/_vimperatorrc
-mklink $HOME$/_bashrc $HOME$/dotfils/_bashrc
-mklink $HOME$/_inputrc $HOME$/dotfils/_inputrc
-mklink $HOME$/_latexmkrc $HOME$/dotfils/_latexmkrc
-exit 0
+mklink "%HOME%/_vimrc" "%HOME%/git/dotfiles/.vimrc"
+mklink "%HOME%/_vimperatorrc" "%HOME%/git/dotfiles/.vimperatorrc"
+mklink "%HOME%/_latexmkrc" "%HOME%/git/dotfiles/.latexmkrc"
