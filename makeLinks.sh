eef98cac84568aabd115b71718ec36e7b557b856
Aoi Kato <machineanswers@gmail.com>
2020-02-17 12:40:48 +0900

    Add installation of i3 config file

--- makeLinks.sh
+++ makeLinks.sh
@@ -7,5 +7,7 @@ cp ~/git/dotfiles/.latexmkrc ~/.latexmkrc
 ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.w3m
 ln -sf ~/git/dotfiles/keymap ~/.w3m/keymap
+mkdir -p ~/.config/i3
+ln -sf ~/git/dotfiles/config ~/.config/i3/config
 
 touch ~/.bashrc.local
