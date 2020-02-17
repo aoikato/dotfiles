2b45320d08f39a1549a8633f386936bfae392938
Aoi Kato <machineanswers@gmail.com>
2020-02-17 13:14:22 +0900

    Add installation of i3status and ranger

--- makeLinks.sh
+++ makeLinks.sh
@@ -9,5 +9,13 @@ mkdir -p ~/.w3m
 ln -sf ~/git/dotfiles/keymap ~/.w3m/keymap
 mkdir -p ~/.config/i3
 ln -sf ~/git/dotfiles/i3config ~/.config/i3/config
+mkdir -p ~/.config/i3status
+ln -sf ~/git/dotfiles/i3status_config ~/.config/i3status/config
+mkdir -p ~/.config/ranger
+ln -sf ~/git/dotfiles/ranger/commands.py ~/.config/ranger/commands.py
+ln -sf ~/git/dotfiles/ranger/commands_full.py ~/.config/ranger/commands_full.py
+ln -sf ~/git/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf ~/git/dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf ~/git/dotfiles/ranger/scope.sh ~/.config/ranger/scope.sh
 
 touch ~/.bashrc.local
