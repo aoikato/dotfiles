d172f9456dff08c1aedd4de17a634362de6b0c01
Aoi Kato <machineanswers@gmail.com>
2020-06-11 09:00:00 +0900

    Function can be omitted

--- makeLinks.sh
+++ makeLinks.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-inst(){
-    SRC=`realpath $1`
-    if [ -z "$2" ];then
-		DST="$HOME/"`basename $1`
-	elif [ -n "`echo $2 | grep -E '.*/$'`" ]; then
-		DST=$2`basename $1`
-		mkdir -p $2
-	else
-		DST=$2
-		mkdir -p `dirname $2`
-	fi
-
-	ln --backup=numbered -s $SRC $DST
-}
-export -f inst
-
-find . -maxdepth 1 -type f | grep -vE 'makeLinks.*|README.md' | xargs -n1 -d'\n' bash -c 'inst "$@"' _
-find ./ranger -maxdepth 1 -type f | xargs -n1 -d'\n' bash -c 'inst "$@" ~/.config/ranger/' _
-inst ~/git/dotfiles/misc/keymap ~/.w3m/
-inst ~/git/dotfiles/misc/i3config ~/.config/i3/config
-inst ~/git/dotfiles/misc/i3statusconfig ~/.config/i3status/config
+ln --backup=numbered -s `find . -maxdepth 1 -type f | grep -vE 'makeLinks.*|README.md' | xargs -n1 realpath` ~/
+[ -e ~/.config/ranger/ ] || mkdir -p ~/.config/ranger/;		ln --backup=numbered -s `find ./ranger -maxdepth 1 -type f | xargs -n1 realpath` ~/.config/ranger/
+[ -e ~/.w3m/ ] || mkdir -p ~/.w3m/;							ln --backup=numbered -s `realpath ./misc/keymap` ~/.w3m/
+[ -e ~/.config/i3/ ] || mkdir -p ~/.config/i3/;				ln --backup=numbered -s `realpath ./misc/i3config` ~/.config/i3/config
+[ -e ~/.config/i3status/ ] || mkdir -p ~/.config/i3status/;	ln --backup=numbered -s `realpath ./misc/i3statusconfig` ~/.config/i3status/config
 
 touch ~/.bashrc.local
