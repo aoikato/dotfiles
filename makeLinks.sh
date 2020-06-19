09da0d7a1de57afed56d8f3d5866f8294eb20f43
Aoi Kato <machineanswers@gmail.com>
2020-06-19 19:45:47 +0900

    Fix bug where other files are linked
    Runnning this sciprt from directories other than one this script belongs
    to causes a malicious behavior where irrelevant files
    are linked. This is because file locations are relative path. To avoid
    this, relative paths are replaced with absolute ones.

--- makeLinks.sh
+++ makeLinks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-ln --backup=numbered -s `find . -maxdepth 1 -type f | grep -vE 'makeLinks.*|README.md' | xargs -n1 realpath` ~/
-[ -e ~/.config/ranger/ ] || mkdir -p ~/.config/ranger/;		ln --backup=numbered -s `find ./ranger -maxdepth 1 -type f | xargs -n1 realpath` ~/.config/ranger/
-[ -e ~/.w3m/ ] || mkdir -p ~/.w3m/;							ln --backup=numbered -s `realpath ./misc/keymap` ~/.w3m/
-[ -e ~/.config/i3/ ] || mkdir -p ~/.config/i3/;				ln --backup=numbered -s `realpath ./misc/i3config` ~/.config/i3/config
-[ -e ~/.config/i3status/ ] || mkdir -p ~/.config/i3status/;	ln --backup=numbered -s `realpath ./misc/i3statusconfig` ~/.config/i3status/config
+
+BASEDIR=`dirname $0`
+ln --backup=numbered -s `find $BASEDIR -maxdepth 1 -type f | grep -vE 'makeLinks.*|README.md' | xargs -n1 realpath` ~/
+[ -e ~/.config/ranger/ ] || mkdir -p ~/.config/ranger/;		ln --backup=numbered -s `find $BASEDIR/ranger -maxdepth 1 -type f | xargs -n1 realpath` ~/.config/ranger/
+[ -e ~/.w3m/ ] || mkdir -p ~/.w3m/;							ln --backup=numbered -s `realpath $BASEDIR/misc/keymap` ~/.w3m/
+[ -e ~/.config/i3/ ] || mkdir -p ~/.config/i3/;				ln --backup=numbered -s `realpath $BASEDIR/misc/i3config` ~/.config/i3/config
+[ -e ~/.config/i3status/ ] || mkdir -p ~/.config/i3status/;	ln --backup=numbered -s `realpath $BASEDIR/misc/i3statusconfig` ~/.config/i3status/config
 
 touch ~/.bashrc.local
